2e81153462e87940e6eef36f96692e88ca7b2c8d
Renato de Almeida Diana <82620563+Griefer-gif@users.noreply.github.com>
2025-09-03 23:44:51 -0300

    literalemnte ta dando erro impossivel de n errar

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -33,7 +33,7 @@ int main(){
                 }else if(option == 1){
                     int x{};
                     cout << "ta meu mano fala quantos graus ta ai em farenthetet:" << endl;  cin >> x; cout << endl << "é isso ai ó " << x * 100 << endl;
-                                                cout << "ihhaaaaaaa";
+                                                cout << "ihhaaaaaaa"
                     
 
                 }
