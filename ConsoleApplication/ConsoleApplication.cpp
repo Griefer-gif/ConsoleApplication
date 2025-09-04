2c6651d3debdad36f63689be8acdfa279442bde2
renato de almeida diana <renato1diana@hotmail.com>
2025-09-03 21:59:47 -0300

    Got the validation thing working, now make the options and shi

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -6,27 +6,35 @@ using namespace std;
 int main()
 {
     bool state = true;
+    short option = 0;
     while (true) {
 
-        short option{};
+        option = 0;
         cout << "Chose an option" << endl
+             << "0. End" << endl
              << "1. Conversion" << endl
-             << "2. Math" << endl;
+             << "2. Math" << endl ;
         cin >> option;
 
-        if (option > 2 || option < 1)
-        {
-            option = 0;
+        if (cin.fail()) {
+            cout << endl << "Se é burro cara" << endl << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+            continue;
         }
         else
         {
-            cout << "Poggers";
-            break;
+            if (option > 2 || option < 0)
+            {
+                cout << endl << "Se é burro cara" << endl << endl;
+                continue;
+            }
+            else {
+                cout << "valeu mano" << endl;
+            }
         }
         
     }
-
-    int option;
 
     return 0;
 }
