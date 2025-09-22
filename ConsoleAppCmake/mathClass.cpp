a2e33235bffbd16b314f9a9fa653b2946cd0131a
Renato de Almeida Diana <82620563+Griefer-gif@users.noreply.github.com>
2025-09-22 20:59:45 -0300

    Convert project to CMAKE

---
+++ ConsoleAppCmake/mathClass.cpp
@@ -0,0 +1,16 @@
+
+float multiply(float x, float y) {
+	return x * y;
+}
+
+float divide(float x, float y) {
+	return x / y;
+}
+
+float square(float x) {
+	return x * x;
+}
+
+float cube(float x) {
+	return (x * x) * x;
+}
